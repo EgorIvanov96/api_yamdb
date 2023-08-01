1356965f19da1052f4064d99f5bcccf4a55dbd3b
Никита Иванов <odigvard@yandex.ru>
2023-08-02 00:03:04 +0300

    Fixed rating serializer

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,17 +1,21 @@
 from rest_framework import serializers
 
-from review.models import Titles
+from review.models import Titles, Review
 
 
 class TitlesSerializer(serializers.ModelSerializer):
-    rating = serializers.SerializerMethodField()
+    class Meta:
+        fields = 'category', 'genre', 'name', 'year'
+        model = Titles
+
+    def get_fields(self):
+        fields = super().get_fields()
+        if self.context['view'].action == 'retrieve':
+            fields['rating'] = serializers.SerializerMethodField()
+        return fields
 
     def get_rating(self, obj):
-        reviews = obj.reviews.all()
-        scores = [review.score for review in reviews]
-        average_rating = sum(scores) / len(scores) if scores else 0
+        reviews = Review.objects.values('score')
+        scores = [review['score'] for review in reviews]
+        average_rating = math.ceil(sum(scores) / len(scores) if scores else 0)
         return average_rating
-
-    class Meta:
-        fields = '__all__'
-        model = Titles
